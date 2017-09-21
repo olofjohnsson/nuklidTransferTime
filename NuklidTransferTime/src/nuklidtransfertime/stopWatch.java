754ba42cb649311bcd99631bafc49276fbc620be
olofjohnsson <olof.s.johnsson@telia.com>
2017-09-21 09:37:28 +0200

    Add files via upload

---
+++ NuklidTransferTime/src/nuklidtransfertime/stopWatch.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuklidtransfertime;
+
+/**
+ *
+ * @author Acer
+ */
+public class stopWatch {
+    double start;
+    
+    public void stopthewatch() {
+        start = 0;
+    } 
+
+    public void start(){
+        start = System.currentTimeMillis();
+    }
+    
+    public double elapsedTime() {
+        if(start == 0) { return 0; } //You need to start it first
+        double time = (double) ((System.currentTimeMillis() - start) / 1000.0);
+        start = 0; // reset start to allow you to start it again later
+        return time;
+    }
+}
