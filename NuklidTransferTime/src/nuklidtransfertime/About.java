c5179947160bdaa177a4e731eb60d349692ecb5f
olojo5 <olojo5@W305050.su.vgregion.se>
2018-09-18 07:49:25 +0200

    v3.0 Nytt versionsnummer

--- NuklidTransferTime/src/nuklidtransfertime/About.java
+++ NuklidTransferTime/src/nuklidtransfertime/About.java
@@ -1,5 +1,5 @@
 /*
-Add logging to backup file
+Lägg till kommentarsfält i användargränssnitt
 */
 package nuklidtransfertime;
 
@@ -14,7 +14,7 @@ import javax.swing.JFrame;
  * About dialog of the Anagram Game application.
  */
 public class About extends JDialog {
-    public String version = "v2.8";
+    public String version = "v3.0";
     /** Creates new form About */
     public About(JFrame parent) {
         super(parent,true);
